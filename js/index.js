b3ef6e59b36df979e64528ce409081a0d6772d74
babladey275 <babladey275@gmail.com>
2024-10-18 15:00:59 +0600

    details show modal done

--- js/index.js
+++ js/index.js
@@ -15,6 +15,7 @@ const loadAllPhones = async (status, searchText) => {
 };
 
 const displayAllPhone = (phones) => {
+  document.getElementById("phones-container").innerHTML = "";
   const phoneContainer = document.getElementById("phones-container");
   phones.forEach((phone) => {
     const { brand, image, slug } = phone;
@@ -31,7 +32,7 @@ const displayAllPhone = (phones) => {
     <h2 class="card-title">${brand}</h2>
     <p>${slug}</p>
     <div class="card-actions">
-      <button class="btn btn-primary">Show Details</button>
+      <button onclick="phoneDetails('${slug}')" class="btn btn-primary">Show Details</button>
     </div>
   </div>
 </div>
@@ -50,6 +51,33 @@ const handleSearch = () => {
   setTimeout(() => {
     loadAllPhones(false, searchText);
   }, 3000);
+};
+
+const phoneDetails = async (slugs) => {
+  const response = await fetch(
+    `https://openapi.programming-hero.com/api/phone/${slugs}`
+  );
+  const data = await response.json();
+
+  const { brand, image, slug } = data.data;
+  const modalContainer = document.getElementById("modal-container");
+  modalContainer.innerHTML = `
+  <dialog id="my_modal_1" class="modal">
+    <div class="modal-box flex flex-col justify-center items-center">
+      <img src="${image}">
+      <h3 class="text-lg font-bold mt-4">${brand}</h3>
+      <p class="py-4">${slug}</p>
+      <div class="modal-action">
+        <form method="dialog">
+          <!-- if there is a button in form, it will close the modal -->
+          <button class="btn">Close</button>
+        </form>
+      </div>
+    </div>
+  </dialog>
+  `;
+
+  my_modal_1.showModal();
 };
 
 loadAllPhones();
